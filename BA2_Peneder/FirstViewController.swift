f800258a9a3d60bedeb3fe2f1467b0055df6926c
Mark Peneder <markpeneder@marks-macbook-pro.local>
2018-02-21 16:37:35 +0100

    Initial Commit

---
+++ BA2_Peneder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BA2_Peneder
+//
+//  Created by Mark Peneder on 21.02.18.
+//  Copyright © 2018 Mark Peneder. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
